99ee71b5a07ce9c8476b8345487ee86bbe022ae9
alsamaedu <alsamsa.educational@gmail.com>
2025-11-17 00:09:46 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,35 +3,27 @@
 //
 
 // 1. استيراد الخدمات الكاملة اللي نحتاجها
+// (هذني جانن ناقصات عندك)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
 import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js";
 
-// 2. !!!!!! أهم خطوة !!!!!!
-// هذا الكود "غلط" (هذا مالتي المثال).
-// "امسحه" وخلي الكود "الحقيقي" اللي جبته من الكونسول مالتك
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD1MKWNuYOrxgdsA4nRGBLWMcQ6RD9Monw", // <<< هذا غلط
-//   authDomain: "alsama-edu.firebaseapp.com",
-//   projectId: "alsama-edu",
-//   storageBucket: "alsama-edu.firebasestorage.app",
-//   messagingSenderId: "1085099490336",
-//   appId: "1:1085099490336:web:6c89d3709494266f0a9045",
-//   measurementId: "G-B43MRSQPMY"
-// };
-
-//
-// الصق (Paste) الكود الحقيقي مالتك هنا
-//
+// 2. هذا الكود (firebaseConfig) اللي أنت دزيته
+// (اللي بيه المفتاح "الغلط" بس راح نستخدمه هسة حتى نتأكد)
 const firebaseConfig = {
-  // ... الكود "الحقيقي" من الكونسول مالتك ...
-  // ... لازم الـ apiKey مالتك يبدي بـ "AIzaSy..." بس يختلف عن الفوك ...
+  apiKey: "AIzaSyD1MKWNuYOrxgdsA4nRGBLWMcQ6RD9Monw",
+  authDomain: "alsama-edu.firebaseapp.com",
+  projectId: "alsama-edu",
+  storageBucket: "alsama-edu.firebasestorage.app",
+  messagingSenderId: "1085099490336",
+  appId: "1:1085099490336:web:6c89d3709494266f0a9045",
+  measurementId: "G-B43MRSQPMY"
 };
 
-
-// 3. تشغيل الخدمات
+// 3. تشغيل الخدمات الكاملة
+// (هذني هم جانن ناقصات عندك)
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app); // خدمة تسجيل الدخول
@@ -41,5 +33,5 @@ const storage = getStorage(app); // خدمة التخزين (للملازم)
 // 4. نخلي المستخدم يبقى مسجل دخوله
 setPersistence(auth, browserLocalPersistence);
 
-// 5. نصدر الخدمات لباقي الملفات
+// 5. نصدر الخدمات لباقي الملفات (أهم خطوة جانت ناقصة)
 export { auth, db, storage };
